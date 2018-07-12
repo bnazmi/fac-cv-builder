4b6641261a35dc23e1aa68ced50fd82b3f74647e
Farah Zaqout <farah.zaqout@gmail.com>
2018-07-12 13:02:01 +0300

    cv gets data from form

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
 import React, { Component , Fragment } from "react";
-// import "./App.css";
-// import { Progress } from "./components/pages/Progress/";
-// import { Header } from "./components/forms/Header/";
-// import { About } from "./components/pages/About/";
-// import { EducationForm } from "./components/pages/EducationForm/";
-// import { ExperienceForm } from "./components/pages/ExperienceForm/";
-// import { SkillsForm } from "./components/pages/SkillsForm/";
-// import { ProtfolioForm } from "./components/pages/ProtfolioForm/";
-// import { SocialForm } from "./components/pages/SocialForm/";
- 
-   import { Header } from "./components/cv/Header";
-   import { About } from "./components/cv/About";
-   import { Skill} from "./components/cv/Skill";
+import "./App.css";
+import { Progress } from "./components/pages/Progress/";
+import { Header } from "./components/forms/Header/";
+import { About } from "./components/pages/About/";
+import { EducationForm } from "./components/pages/EducationForm/";
+import { ExperienceForm } from "./components/pages/ExperienceForm/";
+import { SkillsForm } from "./components/pages/SkillsForm/";
+import { ProtfolioForm } from "./components/pages/ProtfolioForm/";
+import { SocialForm } from "./components/pages/SocialForm/";
+
+   import { Header as CVHeader } from "./components/cv/Header";
+   import { About as CVAbout } from "./components/cv/About";
+   import { Skill as CVSkill } from "./components/cv/Skill";
    import { Experince } from "./components/cv/Experince";
    import { Education } from "./components/cv/Education";
    import { Protofolio } from "./components/cv/Protofolio";
-   import {Footer } from "./components/cv/Footer"
+   import { Footer } from "./components/cv/Footer"
+
 class App extends Component {
   onClick = (ev) => {
     ev.preventDefault();
     const { name, bio, file, country, address, email, phone, educationSchool, fromEducation, toEducation, educationDescription, experinceCompany, experienceCountry, city, titleExperince, fromExperince, toExperince, experinceDescription, skills, projectTitle, projectOverview, portfolioCountry, projectUrl, projectSkills } = ev.currentTarget;
+      this.setState({ newObject: {
+        name: name.value,
+        bio: bio.value,
+        file: file.value,
+        country: country.value,
+        address: address.value,
+        email: email.value,
+        phone: phone.value,
+        educationSchool: educationSchool.value,
+        fromEducation: fromEducation.value,
+        toEducation: toEducation.value,
+        educationDescription: educationDescription.value,
+        experinceCompany: experinceCompany.value,
+        experienceCountry: country.value,
+        city: city.value,
+        titleExperince: titleExperince.value,
+        fromExperince: fromExperince.value,
+        // toExperince, this returns undefined
+        experinceDescription: experinceDescription.value,
+        skills: skills.value,
+        projectTitle: projectTitle.value,
+        projectOverview: projectOverview.value,
+        portfolioCountry: portfolioCountry.value,
+        projectUrl: projectUrl.value,
+        projectSkills: projectSkills.value,
+      } })
 
-     const newObject = {
-      name: name.value,
-      bio: bio.value,
-      file: file.value,
-      country: country.value,
-      address: address.value,
-      email: email.value,
-      phone: phone.value,
-      educationSchool: educationSchool.value,
-      fromEducation: fromEducation.value,
-      toEducation: toEducation.value,
-      educationDescription: educationDescription.value,
-      experinceCompany: experinceCompany.value,
-      experienceCountry: country.value,
-      city: city.value,
-      titleExperince: titleExperince.value,
-      fromExperince: fromExperince.value,
-      // toExperince, this returns undefined
-      experinceDescription: experinceDescription.value,
-      skills: skills.value,
-      projectTitle: projectTitle.value,
-      projectOverview: projectOverview.value,
-      portfolioCountry: portfolioCountry.value,
-      projectUrl: projectUrl.value,
-      projectSkills: projectSkills.value,
-    }
-    console.log(newObject);
+    console.log(this.state.newObject);
   }
 
   classactive=(e)=>{
@@ -73,11 +74,11 @@ class App extends Component {
            item.activepage=false;
          }
 
-      } 
-    
+      }
+
       else{
          item.active = true;
-      } 
+      }
       return item;
     })
     this.setState({
@@ -109,8 +110,13 @@ class App extends Component {
       progress: newProg
     });
   }
+  nextCv=(e)=>{
+    e.preventDefault();
+    this.setState({ cv: true })
+  }
   state = {
     page: 1,
+    cv: false,
     progress: [
       {
         name: "About",
@@ -142,46 +148,52 @@ class App extends Component {
         active:false,
         activepage : false
       }
-    ]
+    ],
+    newObject: {
+    }
   };
 
   render() {
-    // return (
-    //   <Fragment >
-    //     <Header  domain="cvbuilder.co"  />
-    //   <form onClick={this.onClick}>
-    //     <ul id="progressbar">
-    //     {
-    //       this.state.progress.map((item,index)=>{
-    //       if(item.active ){
-    //         return <Progress className="active" key={index} name={item.name} />
-    //       }
-    //       else {
-    //         return <Progress className="" key={index} name={item.name} />
-    //       }
-    //       })
-    //     }
-    //     </ul>
-    //     <About className={this.classactive()[0]} id="about" next={this.next} />
-    //     <EducationForm className={this.classactive()[1]} id="education" next={this.next} previous={this.previous} />
-    //     <ExperienceForm className={this.classactive()[2]} id="experience" next={this.next} />
-    //     <SkillsForm className={this.classactive()[3]}id="skills" next={this.next} />
-    //     <ProtfolioForm className={this.classactive()[4]} id="protfolio" next={this.next} />
-    //     <SocialForm className={this.classactive()[5]} id="social" next={this.next} />
-    //   </form>
-    //   </Fragment>
-    // );
+    if (this.state.cv === false) {
     return (
       <Fragment >
-        <Header domain="cvbuilder.co" />
-        <About name="Abdullah Azmi" bio="web Devloper" image="./../public/me.jpg" />
-        <Skill />
-        <Experince />
-        <Education />
-        <Protofolio />
-        <Footer />
+        <Header  domain="cvbuilder.co"  />
+      <form onClick={this.onClick}>
+        <ul id="progressbar">
+        {
+          this.state.progress.map((item,index)=>{
+          if(item.active ){
+            return <Progress className="active" key={index} name={item.name} />
+          }
+          else {
+            return <Progress className="" key={index} name={item.name} />
+          }
+          })
+        }
+        </ul>
+        <About className={this.classactive()[0]} id="about" next={this.next} />
+        <EducationForm className={this.classactive()[1]} id="education" next={this.next} previous={this.previous} />
+        <ExperienceForm className={this.classactive()[2]} id="experience" next={this.next} />
+        <SkillsForm className={this.classactive()[3]}id="skills" next={this.next} />
+        <ProtfolioForm className={this.classactive()[4]} id="protfolio" next={this.next} />
+        <SocialForm className={this.classactive()[5]} id="social" next={this.nextCv} />
+      </form>
       </Fragment>
     );
+  } else {
+
+      return (
+        <Fragment >
+          <CVHeader domain="cvbuilder.co" />
+          <CVAbout bio={this.state.newObject.bio} image="./../public/me.jpg" name={this.state.newObject.name} />
+          <CVSkill />
+          <Experince />
+          <Education />
+          <Protofolio />
+          <Footer />
+        </Fragment>
+      );
+    }
   }
 }
 
